26ef2ebadd09e5d906d03b849de96abf810e0ee5
MauriPercovich <mauriciopercovich22@gmail.com>
2022-04-02 07:37:46 -0700

    fix comments

--- strtok.c
+++ strtok.c
@@ -4,12 +4,11 @@
 int main()
 {
 	char string[50] = "testing ted tested toc";
-	// Extract the first token
 	char * token = strtok(string, " ");
-	// loop through the string to extract all other tokens
+
 	while( token != NULL )
 	{
-		printf( " %s\n", token ); //printing each token
+		printf( " %s\n", token );
 		token = strtok(NULL, " ");
 	}
 	return 0;
