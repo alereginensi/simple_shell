a173c733f625eb840f1c669d4108f5b752cac978
MauriPercovich <mauriciopercovich22@gmail.com>
2022-04-05 19:18:35 -0700

    changed the printf for a _putchar

--- minishell.c
+++ minishell.c
@@ -1,4 +1,15 @@
 #include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
 
 /**
  * _strcmp - function that concatenates two strings
@@ -40,7 +51,8 @@ int main()
 
 		argv[1] = NULL;
 
-		printf("$ ");
+		_putchar('$');
+		_putchar(' ');
 
 		characters = getline(&buffer, &bufsize, stdin);
 
